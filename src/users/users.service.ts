ca35f0b61ba1eef09e8b0197f9c344d9ae076c60
Petrik Diak <diak@petrik.hu>
2024-12-13 16:24:17 +0100

    tóken

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as argon2 from "argon2";
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private readonly db: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const hashedPw = await argon2.hash(createUserDto.password);
+    const user = await this.db.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPw
+      }
+    })
+    delete user.password;
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
